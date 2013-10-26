eb63c0e1223f4a8668ad5b62602c0ed837613409
Philip <Philip@Philip-PC>
2013-10-26 15:14:12 -0400

    test method and finish

--- src/edu/wpi/first/wpilibj/templates/ElevatorLevelCheck.java
+++ src/edu/wpi/first/wpilibj/templates/ElevatorLevelCheck.java
@@ -3,21 +3,23 @@ package edu.wpi.first.wpilibj.templates;
 //(new Thread(new ElevatorLevelCheck())).start();
 
 import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.templates.Elevator;
 
 public class ElevatorLevelCheck implements Runnable{
 
+    Elevator elevator = Elevator.getInstance();
     public void run(){
         //(L-R)*A= +-DeltaV
         //LV - DeltaV = L
         //RV + DeltaV = R
-        while (!(isInDeadband(currentPosition))){
+        while (true){
             AnalogChannel stringPotL = new AnalogChannel(3);//just a place holder
             AnalogChannel stringPotR = new AnalogChannel(7);//just a place holder
         
             double deltaV;
             double c = 0.05;//modifier to make an acceptable value at which to change the speed of elevator motors
         
-            deltaV = (getDegreesL()-getDegreesR()) * c;
+            deltaV = (elevator.getDegreesL()-elevator.getDegreesR()) * c;
             
             try {
             Thread.sleep(100);
